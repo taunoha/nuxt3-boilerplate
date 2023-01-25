a7e67f9dcf234992571fad6ff13ba46a3321d583
Tauno Hanni <taunoh@gmail.com>
2023-01-25 09:22:17 +0200

    feat: LocalDevOnly component to debug reactive state only in local environment

---
+++ components/LocalDevOnly.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const slots = useSlots();
+
+const container = () => {
+  const slot = slots.default ? slots.default() : null;
+  const config = useRuntimeConfig().public;
+
+  if (config?.environment === "local" && slot) {
+    return h(
+      "div",
+      {
+        class:
+          "text-[10px] p-4 border bg-white shadow-sm border-body/50 rounded-md my-4 mx-2 overflow-hidden overflow-x-auto text-left",
+      },
+      [
+        h(
+          "span",
+          { class: "block text-[8px] uppercase mb-2 text-body/50" },
+          "debug"
+        ),
+        h("pre", slot),
+      ]
+    );
+  }
+};
+</script>
+
+<template>
+  <ClientOnly>
+    <component :is="container"></component>
+  </ClientOnly>
+</template>

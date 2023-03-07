046d12339ccda8a983af26b2f075861784e1e7f6
Tauno Hanni <taunoh@gmail.com>
2023-03-07 12:56:28 +0200

    feat: Add 404 error handling

---
+++ pages/[...slug].vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const route = useRoute();
+setResponseStatus(404);
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <main class="flex grow items-center justify-center">
+      <div class="text-center">
+        <Icon name="logos:nuxt-icon" class="mb-5" size="80"></Icon>
+        <h1 class="font-display mb-6 text-center text-5xl">
+          404 Page Not Found
+        </h1>
+        <p>
+          <small>
+            The requested URL
+            <strong>{{ route.path }}</strong>
+            was not found on this server.
+          </small>
+        </p>
+        <p class="pt-8">
+          <NuxtLink to="/" class="rounded-lg border px-4 py-2">
+            Go back home
+          </NuxtLink>
+        </p>
+      </div>
+    </main>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const { data, error } = await useFetch("/api/v1/mountains");
+const mountains = computed(() => data?.value?.mountains || []);
+useHead({
+  title: "Hello, World!",
+  titleTemplate: "%s - Nuxt3 Boilerplate",
+});
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <main class="flex grow items-center justify-center">
+      <div class="text-center">
+        <Icon name="logos:nuxt-icon" class="mb-5" size="80"></Icon>
+        <h1 class="font-display mb-6 text-center text-5xl">
+          <b class="text-primary p-4 pt-2">Hello, World!</b>
+        </h1>
+        <p class="mb-6">Can you see me?</p>
+        <small class="flex flex-wrap justify-center gap-2">
+          <span v-for="item in mountains" :key="item.title">
+            {{ item.title }}
+          </span>
+          <div v-if="error">{{ error }}</div>
+        </small>
+      </div>
+    </main>
+  </div>
+</template>

9d62afcc997b44017fbebec7e480081d4af3ec64
Tauno Hanni <taunoh@gmail.com>
2024-04-15 21:16:37 +0300

    chore: Node package update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,27 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 import prettier from "eslint-plugin-prettier/recommended";
 
-export default withNuxt({
-  files: ["**/*.ts", "**/*.vue", "*.mjs", "./assets/scss/**/*.scss"],
-  ...prettier,
-});
+export default withNuxt([
+  {
+    files: ["**/*.ts", "**/*.vue", "*.mjs"],
+    ...prettier,
+  },
+  {
+    files: ["*.vue"],
+    rules: {
+      "vue/multi-word-component-names": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          htmlWhitespaceSensitivity: "ignore",
+        },
+      ],
+    },
+  },
+  {
+    files: ["app.vue", "error.vue", "pages/**/*.vue", "layouts/**/*.vue"],
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+  },
+]);

fcef6fb9633cb82c52029c1e80d8c331c8da10bd
Tauno Hanni <taunoh@gmail.com>
2024-09-24 14:20:07 +0300

    chore: Error handling

--- error.vue
+++ error.vue
@@ -4,33 +4,43 @@ interface Props {
 }
 const props = defineProps<Props>();
 
-const message = computed(() => String(props.error?.message || ""));
+const message = computed(() =>
+  String(props.error?.statusMessage || "An error occurred")
+);
+const statusCode = computed(() => props.error?.statusCode || 500);
 const is404 = computed(
-  () => props.error?.statusCode === 404 || message.value.includes("404")
+  () => statusCode.value === 404 || message.value?.includes("404")
+);
+const genericMessage = computed(() =>
+  is404.value
+    ? "Looks like you've followed a broken link or entered a URL that doesn't exist on this site."
+    : "The server was unable to complete your request. Please try again later."
 );
 
 useHead({
-  title: computed(() => (is404.value ? "Page Not Found" : "An error occurred")),
+  title: message.value,
 });
 </script>
 
 <template>
   <div class="flex min-h-dvh flex-col justify-center text-center">
     <main class="container">
-      <Icon name="logos:nuxt-icon" class="mb-5" size="80" />
-      <h1 class="font-display mb-6 text-center text-5xl font-bold">
-        {{ is404 ? "Page Not Found" : "An error occurred" }}
+      <div class="mb-5 text-2xl font-semibold opacity-70">{{ statusCode }}</div>
+      <h1 class="font-display mb-6 text-center text-4xl font-bold">
+        {{ is404 ? message : "An error occurred" }}
       </h1>
-      <p class="mb-12 text-primary">
-        Looks like you've followed a broken link or entered a URL that doesn't
-        exist on this site.
+      <p class="mb-12">
+        {{ genericMessage }}
       </p>
       <NuxtLink
         to="/"
-        class="border-body hover:bg-body rounded-lg border px-4 py-2 transition-colors duration-150 hover:text-white"
+        class="border-body hover:bg-body mb-8 inline-block rounded-lg border px-4 py-2 transition-colors duration-150 hover:bg-white hover:text-black"
       >
         Go back home
       </NuxtLink>
+      <LocalDevOnly>
+        {{ props.error }}
+      </LocalDevOnly>
     </main>
   </div>
 </template>

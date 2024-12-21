ba50330244bb0f23d7029180652e05fa9283e8ec
Tauno Hanni <taunoh@gmail.com>
2024-12-21 11:20:32 +0200

    chore: Node package update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2024-12-20",
   devtools: { enabled: true },
   modules: [
     "@nuxt/devtools",
